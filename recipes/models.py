b34157a8ad37fbcecd4c25c366c130b933d838f3
Matheus Saraiva <saraivarego@gmail.com>
2023-05-04 20:50:09 -0300

    admin

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -21,4 +24,7 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/corvers/%y/%m/%d')
     Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
 
