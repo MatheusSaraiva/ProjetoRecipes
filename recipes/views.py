bc578d270c114d6639afcf74512f1226d8711039
Matheus Saraiva <saraivarego@gmail.com>
2023-05-05 17:08:15 -0300

    acrecentando 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,11 @@
 from django.http import Http404
-from django.shortcuts import get_list_or_404, render
-
-from utils.recipes.factory import make_recipe
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = recipes = Recipe.objects.filter(is_published=True, ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -24,7 +22,8 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(pk=id, is_published=True).order_by('-id').first()
+
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
