7216b47e4adcf7127dd52853ecf7808c21f41838
Matheus Saraiva <saraivarego@gmail.com>
2023-05-22 18:38:13 -0300

    Class Based Views

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -25,14 +27,24 @@ class Recipe(models.Model):
     cover = models.ImageField(
         upload_to="recipes/covers/%Y/%m/%d/", blank=True, default=""
     )
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(
-        User,
+    category = models.ForeignKey(
+        Category,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
         default=None,
     )
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("recipes:recipe", args=(self.id,))
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f"{slugify(self.title)}"
+            self.slug = slug
+
+        return super().save(*args, **kwargs)
