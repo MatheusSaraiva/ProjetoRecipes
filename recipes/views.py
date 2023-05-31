883e84ed2fd4896133f6545ffbeb0bf763e89f6b
Matheus Saraiva <saraivarego@gmail.com>
2023-05-31 17:51:21 -0300

    profile

--- recipes/views.py
+++ recipes/views.py
@@ -23,7 +23,7 @@ class RecipeListViewBase(ListView):
         qs = qs.filter(
             is_published=True,
         )
-        qs = qs.select_related("author", "category")
+        qs = qs.select_related("author", "category", "author__profile")
         return qs
 
     def get_context_data(self, *args, **kwargs):
